48473011abf3c17cf8d647bd06b6945ff42105e3
unknown <shakthinakkeeran@gmail.com>
2023-02-26 23:48:38 +0530

    first commit

---
+++ src/main/java/com/makemytrip/MakeMyTrip/App.java
@@ -0,0 +1,13 @@
+package com.makemytrip.MakeMyTrip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
